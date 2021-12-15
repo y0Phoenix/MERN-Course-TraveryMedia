250868d31c10c8af0da037e877a97d082dd278cd
Aaron <aarongraybill3@gmail.com>
2021-12-14 21:17:26 -0600

    init user shcema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mon = require('mongoose');
+
+const UserSchema = new new mon.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = User = mon.Mongoose.module('user', UserSchema);
