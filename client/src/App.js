0e0d07dc9d90433efa1d2ad53d613a94883710ff
Aaron <aarongraybill3@gmail.com>
2021-12-17 21:52:12 -0600

    init react app with register and login routes

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+// import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+        <section className='container'>
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,17 +9,17 @@ const connectDB = require('./config/db');
 connectDB();
 
 // init middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => res.send('API Running'));
 
 // routes
-app.use('/api/users', require('./routes/apis/users'))
-app.use('/api/auth', require('./routes/apis/auth'))
-app.use('/api/profile', require('./routes/apis/profile'))
-app.use('/api/post', require('./routes/apis/post'))
+app.use('/api/users', require('./routes/apis/users'));
+app.use('/api/auth', require('./routes/apis/auth'));
+app.use('/api/profile', require('./routes/apis/profile'));
+app.use('/api/post', require('./routes/apis/post'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
-
