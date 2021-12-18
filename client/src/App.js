10e8af9fa5addee4ea9773488f22872373f182ec
Aaron <aarongraybill3@gmail.com>
2021-12-18 00:02:52 -0600

    init add password not match alert

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // redux
@@ -19,6 +20,7 @@ const App = () => {
             <Route path='/' element={<Landing />} />
           </Routes>
           <section className='container'>
+            <Alert />
             <Routes>
               <Route path='/register' element={<Register />} />
               <Route path='/login' element={<Login />} />
